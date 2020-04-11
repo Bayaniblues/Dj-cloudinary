3181a4ab6911cf70755dc76c79f7d5d89703dfdf
Bayaniblues <tez.ridge2012@gmail.com>
2020-04-10 20:18:31 -0600

    For new user registration

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 In development
 
-A starter project to grow other projects on top of.
+A starter project to grow other django projects on top of.
 
     pip install django == 2.0.5
     
 Features Materialize and Cloudinary for Heroku deployment. 
+
+    TODO: 
+    SMTP routing
+    
+The Registration workflow is Forms => views => templates
--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -16,5 +16,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
+    # Register URls
+    path('register/', views.register, name='register')
 
 ]
